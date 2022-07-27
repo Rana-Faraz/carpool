0a829bfebc2daadb39a9d3d93320fb9dd7c78a98
Talha-Farrukh <talhaf569@gmail.com>
2022-07-27 12:41:11 +0500

    Casecading dropdown added in getRide screen

--- screens/GetRideScreen.js
+++ screens/GetRideScreen.js
@@ -1,10 +1,11 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
+import SelectDropdown from "react-native-select-dropdown";
 import { lightModColor } from "../style/Color";
-import { btn, btnText, itemCenter } from "../style/Style";
+import { btn, btnText, dropDownStyle, itemCenter } from "../style/Style";
 
 const GetRideScreen = () => {
   const Navigation = useNavigation();
@@ -14,14 +15,59 @@ const GetRideScreen = () => {
   const [pickUp, setPickUp] = useState();
   const [dropIn, setDropIn] = useState();
 
-  const Data = [
-    { label: "Lahore", value: "Lahore" },
-    { label: "Islamabad", value: "Islamabad" },
-    { label: "Karachi", value: "Karachi" },
-    { label: "Faisalabad", value: "Faisalabad" },
-    { label: "Multan", value: "Multan" },
-    { label: "Quetta", value: "Quetta" },
+  // ************** Casecading Drop Down Logic ***************
+
+  const pickupDropDown = [
+    "Lahore",
+    "Islamabad",
+    "Rawalpindi",
+    "Karachi",
+    "Faisalabad",
+    "Gujranwala",
+    "Sialkot",
+    "Sheikhupura",
+    "Okara",
+    "Pattoki",
+    "Kasur",
+    "Multan",
+    "Peshawar",
+    "Quetta",
   ];
+  const dropinDropDown = [
+    "Lahore",
+    "Islamabad",
+    "Rawalpindi",
+    "Karachi",
+    "Faisalabad",
+    "Gujranwala",
+    "Sialkot",
+    "Sheikhupura",
+    "Okara",
+    "Pattoki",
+    "Kasur",
+    "Multan",
+    "Peshawar",
+    "Quetta",
+  ];
+  const [pickDropValue, setPickDropValue] = useState(pickupDropDown);
+  const [dropDropValue, setDropDropValue] = useState(dropinDropDown);
+
+  const handlePickup = () => {
+    return pickupDropDown.filter((loc) => loc !== dropIn);
+  };
+  const handleDropin = () => {
+    return dropinDropDown.filter((loc) => loc !== pickUp);
+  };
+
+  useLayoutEffect(() => {
+    setPickDropValue(handlePickup);
+    console.log("pick" + pickDropValue);
+  }, [dropIn]);
+
+  useLayoutEffect(() => {
+    setDropDropValue(handleDropin);
+    console.log("drop" + dropDropValue);
+  }, [pickUp]);
 
   return (
     <View style={[itemCenter, { flex: 1 }]}>
@@ -33,55 +79,43 @@ const GetRideScreen = () => {
         }}
       >
         <View style={[itemCenter, { flexDirection: "row" }]}>
-          <View style={{ width: "25%" }}>
-            <MaterialIcons
-              name="my-location"
-              size={25}
-              color={lightModColor.themeBackground}
-            />
-          </View>
-          <View style={{ width: "40%" }}>
-            <RNPickerSelect
-              placeholder={{ label: "Pick Up" }}
-              items={Data}
-              onValueChange={(value) => {
-                setPickUp(value);
-              }}
-              style={{
-                inputIOS: {
-                  transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }],
-                },
-                inputAndroid: {
-                  transform: [{ scaleX: 1.4 }, { scaleY: 1.4 }],
-                },
-              }}
+          <View style={{ width: "70%" }}>
+            <SelectDropdown
+              data={pickDropValue}
+              buttonStyle={dropDownStyle}
+              buttonTextStyle={{ fontSize: 15 }}
+              // dropdownStyle={{ height: "70%" }}
+              dropdownIconPosition="left"
+              defaultValue={pickUp}
+              renderDropdownIcon={() => (
+                <MaterialIcons
+                  name="my-location"
+                  size={20}
+                  color={lightModColor.themeBackground}
+                />
+              )}
+              onSelect={(text) => setPickUp(text)}
             />
           </View>
         </View>
         <Text>To</Text>
         <View style={[itemCenter, { flexDirection: "row", marginBottom: 20 }]}>
-          <View style={{ width: "25%" }}>
-            <MaterialIcons
-              name="location-on"
-              size={25}
-              color={lightModColor.themeBackground}
-            />
-          </View>
-          <View style={{ width: "40%" }}>
-            <RNPickerSelect
-              placeholder={{ label: "Drop" }}
-              items={Data}
-              onValueChange={(value) => {
-                setDropIn(value);
-              }}
-              style={{
-                inputAndroid: {
-                  transform: [{ scaleX: 1.4 }, { scaleY: 1.4 }],
-                },
-                inputIOS: {
-                  transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }],
-                },
-              }}
+          <View style={{ width: "70%" }}>
+            <SelectDropdown
+              data={dropDropValue}
+              buttonStyle={dropDownStyle}
+              buttonTextStyle={{ fontSize: 15 }}
+              // dropdownStyle={{ height: "50%" }}
+              dropdownIconPosition="left"
+              // defaultValue={dropIn}
+              renderDropdownIcon={() => (
+                <MaterialIcons
+                  name="location-on"
+                  size={20}
+                  color={lightModColor.themeBackground}
+                />
+              )}
+              onSelect={(text) => setDropIn(text)}
             />
           </View>
         </View>
